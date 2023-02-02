5db0e445cfedd8694298c3a7a88e081d702f0cc0
Mariia Kryzhalko <kruzhalko93@gmail.com>
2023-02-02 11:47:04 +0200

    completed task

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+
+function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <li className={css.item}>
+      <span className={css[isOnline]}>{isOnline}</span>
+      <img className={css.avatar} src={avatar} alt={name} width="28" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+export default FriendListItem;
